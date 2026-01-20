750f6b62e92068e53d1adac864c65eac470bfaba
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2026-01-20 22:49:14 +0100

    feat: add CORS

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import create_db_and_tables
 
 from certificates import router as certificates_router
 
+origins = os.getenv("CORS_ORIGINS", "http://localhost:3000").split(",")
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(certificates_router)
 
