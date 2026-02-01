e78f2c254b325461e0df131ed9437270b71af51a
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2026-02-01 15:12:42 +0100

    feat: order certificates by date in get endpoint

--- certificates/router.py
+++ certificates/router.py
@@ -9,22 +9,27 @@ router = APIRouter(prefix="/certificates", tags=["certificates"])
 
 @router.get("/", response_model=list[CertificateRead])
 def read_certificates():
-    response = supabase.table("certificates").select("*").execute()
+    response = (
+        supabase.table("certificates").select("*").order("date", desc=True).execute()
+    )
     return response.data
+
 
 @router.post("/", tags=["certificates"], response_model=list[CertificateRead])
 def create_certificate(
     certificate: CertificateCreate,
     _: str = Depends(get_api_key),
 ):
-    response = supabase.table("certificates").insert(certificate.model_dump(mode="json")).execute()
+    response = (
+        supabase.table("certificates")
+        .insert(certificate.model_dump(mode="json"))
+        .execute()
+    )
     return response.data
 
 
 @router.delete("/{id}", tags=["certificates"])
-def delete_certificate(
-    id: int, _: str = Depends(get_api_key)
-):
+def delete_certificate(id: int, _: str = Depends(get_api_key)):
     response = supabase.table("certificates").delete().eq("id", id).execute()
     if not response.data:
         raise HTTPException(status_code=404, detail="Certificate not found")
